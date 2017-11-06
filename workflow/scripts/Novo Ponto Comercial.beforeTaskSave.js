2bc4c6e0d4904f7176193c1c9b33d5b080c6a79d
caio-fluig <32585480+caio-fluig@users.noreply.github.com>
2017-11-06 10:26:51 -0200

    Process Added
    Process Added

---
+++ workflow/scripts/Novo Ponto Comercial.beforeTaskSave.js
@@ -0,0 +1,17 @@
+function beforeTaskSave(colleagueId,nextSequenceId,userList){
+	
+	/*var attachments = hAPI.listAttachments();
+    var hasAttachment = false;
+  
+    for (var i = 0; i < attachments.size(); i++) {
+        var attachment = attachments.get(i);
+        if (attachment.getDocumentDescription() == "Proposta.pdf") {
+            hasAttachment = true;
+        }
+    }
+  
+    if (!hasAttachment) {
+        throw "Proposta comercial não foi anexada.";
+    }
+    */
+}
